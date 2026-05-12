3f1d456e5a3903ac6a4fa68396bf8266e320a0fa
Abhilash Jha <abhilashjha2004@gmail.com>
2026-05-12 17:27:01 +0530

    Add railway-mysql

--- server/config/database.js
+++ server/config/database.js
@@ -9,6 +9,7 @@ const sequelize = new Sequelize(
     process.env.DB_PASSWORD || 'root',
     {
         host: process.env.DB_HOST || '127.0.0.1',
+        port: process.env.DB_PORT || 3306,
         dialect: 'mysql',
         logging: false, // Set to console.log to see SQL queries
         pool: {
